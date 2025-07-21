b932a15db993a80f70bcb2970e4833416034c1fd
Dhany Noor Alfian <dhanyalvian@gmail.com>
2025-07-21 20:19:22 +0700

    New Commit: - add Navbar - fix layouting homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,48 @@
 //- app/page.tsx
 
 import FeatureSection from "@/components/feature-section";
+import Navbar from "@/components/navbar";
 import TestimonialSection from "@/components/testimonial-section";
 import Image from "next/image";
 
 const SmartWatchPage = () => {
   return (
-    <div className="container mx-auto px-4 py-12">
-      <section className="text-center">
-        <Image
-          src="/images/apple-watch-se-500x395.png"
-          alt="Apple Watch SE"
-          width={500}
-          height={395}
-          className="mx-auto mb-8"
-        />
-        <h1 className="text-5xl font-extrabold tracking-tight text-lucide-title">
-          Apple Watch SE
-        </h1>
-        <p className="mt-4 max-w-2xl mx-auto text-xl text-lucide-subtitle">
-          Kemampuan memukau yang terjangkau.
-        </p>
+    <div className="">
+      <Navbar />
 
-        <button className="
-          mt-8 px-8 py-3
-          bg-lucide-red
-          text-white font-semibold
-          cursor-pointer transition-colors
-          rounded-full
-          shadow-lg hover:shadow-xl
-        ">
-          Beli Sekarang
-        </button>
-      </section>
-      
-      <FeatureSection />
-      
-      <TestimonialSection />
+      <div className="container mx-auto px-4 py-12 pt-24">
+        <section className="text-center">
+          <Image
+            rel="preload"
+            src="/images/apple-watch-se-500x395.png"
+            alt="Apple Watch SE"
+            width={500}
+            height={395}
+            className="mx-auto mb-8"
+          />
+          <h1 className="text-5xl font-extrabold tracking-tight text-lucide-title">
+            Apple Watch SE
+          </h1>
+          <p className="mt-4 max-w-2xl mx-auto text-xl text-lucide-subtitle">
+            Kemampuan memukau yang terjangkau.
+          </p>
+
+          <button className="
+            mt-8 px-8 py-3
+            bg-lucide-red
+            text-white font-semibold
+            cursor-pointer transition-colors
+            rounded-full
+            shadow-lg hover:shadow-xl
+          ">
+            Beli Sekarang
+          </button>
+        </section>
+
+        <FeatureSection />
+
+        <TestimonialSection />
+      </div>
     </div>
   );
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="
+      fixed flex
+      px-5 py-3 z-50
+      w-full
+      justify-between
+      border-b border-b-lucide-border
+      bg-white bg-opacity-50 backdrop-blur-xl
+      shadow-xs
+    ">
+      <div className="container mx-auto md:px-4 relative flex items-center justify-between">
+        <div className="flex flex-1 font-lg font-bold text-lucide-black">
+          Apple Watch SE
+        </div>
+        
+        <div className="absolute inset-y-0 right-0 flex items-center sm:static sm:inset-auto sm:ml-6 sm:pr-0n mx-auto">
+          <button className="
+            px-3 py-1
+            bg-lucide-red
+            text-white text-xs
+            cursor-pointer transition-colors
+            rounded-full
+            shadow-md
+          ">
+            Beli
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
